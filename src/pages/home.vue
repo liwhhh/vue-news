f3821c75f7df0e7098fb13ced6d633c1ff1334f2
lwhong <li@whong.com>
2019-10-30 17:08:18 +0800

    创建了一个空白首页,能跳转到个人中心

---
+++ src/pages/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div @click="sendProfile">个人中心</div>
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+export default {
+   methods:{
+     sendProfile(){
+       this.$router.push({
+         name:'profilePage'
+       })
+     }
+   }
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,15 @@ import HelloWorld from '@/components/HelloWorld'
 import Login from '@/pages/login.vue';//登录页
 import Register from '@/pages/Register.vue';//注册页
 import Profile from '@/pages/profile.vue';//个人中心页
+import Home from '@/pages/home.vue';//个人中心页
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'homePage',
+      component: Home
     },
     {
       path: '/login',
