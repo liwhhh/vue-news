b091240b40fcaf79e5f37aa34b617c0d1cb4c8a2
lwhong <li@whong.com>
2019-10-30 18:10:54 +0800

    编辑页面布局完成

---
+++ src/components/headerEdit.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <span class="iconfont iconjiantou2"></span>
+    <div class="text">{{label}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['label']
+}
+</script>
+
+<style lang="less" scoped>
+ .header{
+   padding: 20px;
+   display: flex;
+   justify-content: space-between;
+    .text{
+      flex: 1;
+      text-align: center;
+      font-weight: bold;
+    }
+ }
+</style>
--- src/pages/editprofile.vue
+++ src/pages/editprofile.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>编辑页</div>
+  <div>
+    <headerEdit label="编辑资料"></headerEdit>
+    <div class="avatar">
+      <img src="/static/images/hu.png" alt="">
+    </div>
+    <sonCell label="昵称" desc="火星网友" @inptBtn="setNickName"></sonCell>
+    <sonCell label="密码" desc="****" @inptBtn="setPassWord"></sonCell>
+    <sonCell label="性别" desc="男" @inptBtn="setGender"></sonCell>
+  </div>
 </template>
 
 <script>
+import headerEdit from '../components/headerEdit.vue';
+import sonCell from '../components/soncell.vue';
 export default {
+    components:{
+      headerEdit:headerEdit,
+      sonCell:sonCell
+    },
+    methods:{
+      setNickName(){//设置昵称
+        alert('加油')
+      },
+      setPassWord(){
+        alert('加油')
 
+      },
+      setGender(){
+        alert('加油')
+
+      }
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+ .avatar{
+   width: 100%;
+   height: 100%;
+   display: flex;
+   justify-content: center;
+   img{
+     margin:13.889vw 0;
+     width: 19.444vw;
+     height: 19.444vw;
+     border-radius: 50%;
+   }
+ }
 </style>
