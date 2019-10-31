c935f71e5bf9e882e97e5fc4ddd4a6da448760e4
lwhong <li@whong.com>
2019-10-31 17:47:28 +0800

    文章列表适配视频组件样式页面

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
+     
+      <!-- 添加一个视频的模板 -->
+        <div class="videoPost" v-if="item.type == 2">
+            <div class="title">{{item.title}}</div>
+            <div class="cover">
+                <img :src="item.cover[0].url" alt="" class="coverImg">
+                <div class="btnWrapper">
+                    <div class="iconfont iconshipin"></div>
+                </div>
+            </div>
+            <div class="info">{{item.user.nickname}} {{item.comment_length}}跟帖</div>
+        </div>
+
+
+
     <!-- 1~2张会显示 -->
-    <div class="singleIngPost" v-if="item.cover.length > 0 && item.cover.length < 3">
+    <div class="singleIngPost" v-else-if="item.cover.length > 0 && item.cover.length < 3">
       <div class="left">
         <div class="title">{{item.title}}</div>
         <div class="info">{{item.user.nickname}} {{item.comment_length}}跟帖</div>
@@ -10,7 +25,7 @@
     </div>
 
     <!-- 3张图片的话 -->
-    <div class="multipImgPost" v-if="item.cover.length >= 3">
+    <div class="multipImgPost" v-else-if="item.cover.length >= 3">
       <div class="title">{{item.title}}</div>
       <div class="meilles">
         <img :src="item.cover[0].url" alt class="coverImg" />
@@ -34,7 +49,7 @@
     justify-content: space-between;
     align-items: center;
     padding: 2.778vw;
-    border: 1px solid #eee;
+    border-bottom: 1px solid #eee;
     .left {
       flex: 1;
       height: 20.833vw;
@@ -64,7 +79,7 @@
     }
   }
 
-  //3张
+  //3张图片
   .multipImgPost {
     padding: 2.778vw;
     border: 1px solid #eee;
@@ -91,6 +106,54 @@
       overflow: hidden;
       font-size: 14px;
       color: #888;
+    }
+  }
+  //视频样式
+  .videoPost{
+    padding: 2.778vw;
+    border-bottom: 1px solid #eee;
+    .title{
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+    }
+    .info{
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+      font-size: 14px;
+      color: #888;
+    }
+    .cover{
+      margin: 2.778vw 0;
+      position: relative;
+      .coverImg{
+        width: 100%;
+        height: 47.222vw;
+        object-fit: cover;//截取中间部分
+      }
+      .btnWrapper{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .iconfont{
+          width: 12.778vw;
+          height: 12.778vw;
+          text-align: center;
+          line-height: 12.778vw;
+          background: #ccc;
+          color:#fff;
+          font-size: 12.778vw;
+          border-radius: 6.667vw;
+        }
+      }
     }
   }
 </style>
