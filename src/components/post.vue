544960b684230dad506218d0b67cbe74f9c57276
lwhong <li@whong.com>
2019-10-31 15:30:30 +0800

    文章列表组件适配多张图片样式完成

--- src/components/post.vue
+++ src/components/post.vue
@@ -1,53 +1,96 @@
 <template>
   <div>
-    <div class="singleIngPost">
-        <div class="left">
-           <div class="title">{{item.title}}</div>
-           <div class="info">{{item.user.nickname}} {{item.comment_length}}跟帖</div>
-        </div>
-        <img :src="item.cover[0].url" alt="" class="imgRight" />
+    <!-- 1~2张会显示 -->
+    <div class="singleIngPost" v-if="item.cover.length > 0 && item.cover.length < 3">
+      <div class="left">
+        <div class="title">{{item.title}}</div>
+        <div class="info">{{item.user.nickname}} {{item.comment_length}}跟帖</div>
+      </div>
+      <img :src="item.cover[0].url" alt class="imgRight" />
+    </div>
+
+    <!-- 3张图片的话 -->
+    <div class="multipImgPost" v-if="item.cover.length >= 3">
+      <div class="title">{{item.title}}</div>
+      <div class="meilles">
+        <img :src="item.cover[0].url" alt class="coverImg" />
+        <img :src="item.cover[1].url" alt class="coverImg" />
+        <img :src="item.cover[2].url" alt class="coverImg" />
+      </div>
+      <div class="infos">{{item.user.nickname}} {{item.comment_length}}跟帖</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props:['item']
-}
+  export default {
+    props: ["item"]
+  };
 </script>
 
 <style lang="less" scoped>
-.singleIngPost{
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2.778vw;
-  border: 1px solid #eee;
-  .left{
-    flex: 1;
-    height: 20.833vw;
+  .singleIngPost {
     display: flex;
-    flex-direction: column;
     justify-content: space-between;
-    padding-right:2.778vw;
-  }
-  .title{ //两行就...
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 2;
-    overflow: hidden;
+    align-items: center;
+    padding: 2.778vw;
+    border: 1px solid #eee;
+    .left {
+      flex: 1;
+      height: 20.833vw;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding-right: 2.778vw;
     }
-    .info{
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 1;
-    overflow: hidden;
-    color: #888;
+    .title {
+      //两行就...
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
     }
-  .imgRight{
-    object-fit: cover;//自动截取中间部分内容,不变形
-    width: 33.611vw;
-    height: 20.833vw;
+    .info {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+      color: #888;
+    }
+    .imgRight {
+      object-fit: cover; //自动截取中间部分内容,不变形
+      width: 33.611vw;
+      height: 20.833vw;
+    }
   }
-}
+
+  //3张
+  .multipImgPost {
+    padding: 2.778vw;
+    border: 1px solid #eee;
+    .title {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
+    .meilles {
+      display: flex;
+      padding: 2.778vw;
+      justify-content: space-around;
+      .coverImg {
+        width: 33%;
+        height: 20.556vw;
+        object-fit: cover;
+      }
+    }
+    .infos {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 1;
+      overflow: hidden;
+      font-size: 14px;
+      color: #888;
+    }
+  }
 </style>
