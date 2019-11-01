36df33910d59784ef9280a52e282e3da509b5a36
lwhong <li@whong.com>
2019-11-01 14:27:54 +0800

    新闻详情,组件的头部关注与已关注样式完成

---
+++ src/components/postDetailHeader.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="wrapper">
+
+    <div class="follow" v-if="!isShow">
+      <div class="left" >
+        <span class="iconfont iconjiantou2"></span>
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="right">关注</div>
+    </div>
+
+    <div class="unfollow"  v-if="isShow">
+      <div class="left">
+        <span class="iconfont iconjiantou2"></span>
+        <span class="iconfont iconnew"></span>
+      </div>
+      <div class="right">已关注</div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isShow: true
+      };
+    }
+  };
+</script>
+
+<style lang="less" scoped>
+  .unfollow {
+    padding: 2.778vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .iconnew {
+        font-size: 12.5vw;
+        padding-left: 1.667vw;
+      }
+    }
+    .right {
+      width: 17.222vw;
+      font-size: 13px;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 13px;
+      border: 0.278vw solid #888;
+      color: #333;
+    }
+  }
+  .follow {
+    padding: 2.778vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .iconnew {
+        font-size: 12.5vw;
+        padding-left: 1.667vw;
+      }
+    }
+    .right {
+      width: 17.222vw;
+      font-size: 13px;
+      text-align: center;
+      padding: 5px 0;
+      border-radius: 4.167vw;
+      background-color: red;
+      border: 0.278vw solid red;
+      color: #fff;
+    }
+  }
+</style>
--- src/pages/postdetail.vue
+++ src/pages/postdetail.vue
@@ -1,14 +1,18 @@
 <template>
-  <div>新闻详情/文章详情</div>
+  <postDetailHeader/>
+
 </template>
 
 <script>
+import postDetailHeader from '../components/postDetailHeader.vue';
 export default {
+  components:{
+    postDetailHeader
+  },
      data(){
        return{
          //得到主页的动态 路由 
          postId:this.$route.params.id,
-
        }
      }
 }
